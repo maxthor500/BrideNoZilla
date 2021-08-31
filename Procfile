3933883a7a071849826d1e1007287c0fba4bbfdd
maxthor500 <iraubuntu@gmail.com>
2021-08-31 22:10:09 +0100

    Deploy to Heroku part 1 - setup database and allowed hosts

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bride_no_zilla.wsgi:application
--- bride_no_zilla/settings.py
+++ bride_no_zilla/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)a($#2phu9)&(ojjuuzn*%ql_**%g^7gj=9woq8xd3tr1o+zq4'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['bride-no-zilla.herokuapp.com', 'localhost']
 
 # Application definition
 
@@ -122,12 +122,18 @@ WSGI_APPLICATION = 'bride_no_zilla.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
