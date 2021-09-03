9b172249aa87ce2f41b9f62eee0825cf7a4c253e
maxthor500 <iraubuntu@gmail.com>
2021-09-03 17:34:19 +0100

    Create contact and about templates

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('contact', views.contact_me, name='contact'),
+    path('about', views.about_me, name="about")
 ]
--- home/views.py
+++ home/views.py
@@ -6,3 +6,13 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page. """
     return render(request, 'home/index.html')
+
+
+def contact_me(request):
+    """ A view to return the contact me page. """
+    return render(request, 'home/contact.html')
+
+
+def about_me(request):
+    """ A view to return the about me page. """
+    return render(request, 'home/about.html')
